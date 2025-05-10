112e0264760683d06234dbc91b13f073af228bd3
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2025-05-10 16:29:15 +0900

    RSpecを導入し、ci.ymlのテストをRSpecで実行するよう修正

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'validations' do
+    it 'is valid with a unique email_address and password' do
+      user = User.new(email_address: 'test@example.com', password: 'password123')
+      expect(user).to be_valid
+    end
+  end
+end
